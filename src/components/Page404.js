87c761308693c498606267c79e9551291351c83e
Miguel(MgARL) <mgarlcr@gmail.com>
2022-04-01 23:20:52 -0400

    Added content to 404page

--- src/components/Page404.js
+++ src/components/Page404.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import confusedIMG from '../images/Link_confused.jpg'
+import Container from 'react-bootstrap/Container'
 
 function Page404() {
   return (
-    <div>
-        <h1>Page Not Found</h1>
-    </div>
+    <Container>
+        <h1>Page Not Found Error code 404</h1>
+        <img src={confusedIMG} alt="Confused Link" />
+        <p>We are sorry we could find that item or page</p>
+    </Container>
   )
 }
 
